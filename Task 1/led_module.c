9ae12a09a30b38dec87dcada083197bd6ea00ec6
cho4u4o <cho4u4o@gmail.com>
2024-12-01 17:59:30 +0900

    feat : 스레드 추가

--- Task 1/led_module.c
+++ Task 1/led_module.c
@@ -4,6 +4,8 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
 
 #define NUM_LEDS 4
 #define NUM_SWITCHES 4
@@ -12,12 +14,131 @@ static int led_pins[NUM_LEDS] = {23, 24, 25, 1};
 static int switch_pins[NUM_SWITCHES] = {4, 17, 27, 22};
 static int irq_numbers[NUM_SWITCHES];
 
-volatile int mode = 0;
+volatile int mode = -1;
 volatile int individual_direction = 0;
+static struct task_struct *led_thread = NULL;
 
-static void reset_leds(void);
-static void set_led(int led_idx, int value);
-static irqreturn_t switch_handler(int irq, void *dev_id);
+// 스레드 함수
+static int led_thread_function(void *data);
+
+static void reset_leds(void) {
+    int i;
+    for (i = 0; i < NUM_LEDS; i++) {
+        gpio_set_value(led_pins[i], 0);
+    }
+}
+
+static void set_led(int led_idx, int value) {
+    if (led_idx >= 0 && led_idx < NUM_LEDS) {
+        gpio_set_value(led_pins[led_idx], value);
+    }
+}
+
+// LED 스레드 함수
+static int led_thread_function(void *data) {
+    int i;
+    
+    while (!kthread_should_stop()) {
+        switch (mode) {
+            case 0: // 전체 모드
+                if (mode == 0) {
+                    for (i = 0; i < NUM_LEDS; i++) {
+                        set_led(i, 1);
+                    }
+                    printk(KERN_INFO "All LEDs ON\n");
+                    msleep(2000);
+
+                    reset_leds();
+                    printk(KERN_INFO "All LEDs OFF\n");
+                    msleep(2000);
+                }
+                break;
+
+            case 1: // 개별 모드
+                reset_leds();
+                if (mode == 1) {
+                    if (individual_direction == 0) {
+                        for (i = 0; i < NUM_LEDS; i++) {
+                            if (mode != 1) break;
+                            set_led(i, 1);
+                            printk(KERN_INFO "LED[%d] ON\n", i);
+                            msleep(2000);
+                            reset_leds();
+                        }
+                    } else {
+                        for (i = NUM_LEDS - 1; i >= 0; i--) {
+                            if (mode != 1) break;
+                            set_led(i, 1);
+                            printk(KERN_INFO "LED[%d] ON\n", i);
+                            msleep(2000);
+                            reset_leds();
+                        }
+                    }
+                    individual_direction = !individual_direction;
+                    printk(KERN_INFO "Direction reversed: %s\n",
+                           individual_direction ? "Right-to-Left" : "Left-to-Right");
+                }
+                break;
+
+            default:
+                msleep(100); 
+                break;
+        }
+        
+        // 스레드가 종료되어야 하는지 주기적으로 확인
+        if (kthread_should_stop())
+            break;
+    }
+    
+    return 0;
+}
+
+static irqreturn_t switch_handler(int irq, void *dev_id) {
+    int switch_id = (int)(long)dev_id;
+
+    printk(KERN_INFO "Switch %d pressed\n", switch_id);
+
+    // 스레드 관리
+    if (led_thread) {
+        kthread_stop(led_thread);
+        led_thread = NULL;
+    }
+
+    mode = switch_id;
+
+    switch (mode) {
+        case 0:
+        case 1:
+            // 새 스레드 생성
+            led_thread = kthread_run(led_thread_function, NULL, "led_control_thread");
+            if (IS_ERR(led_thread)) {
+                printk(KERN_ERR "Failed to create LED control thread\n");
+                led_thread = NULL;
+            }
+            break;
+
+        case 2:
+            if (switch_id >= 0 && switch_id < 3) {
+                int led_index = switch_id;
+                int current_state = gpio_get_value(led_pins[led_index]);
+                set_led(led_index, !current_state);
+                printk(KERN_INFO "Manual mode: LED[%d] toggled to %d\n", led_pins[led_index], !current_state);
+            } else {
+                printk(KERN_WARNING "Manual mode: Invalid switch_id %d\n", switch_id);
+            }
+            break;
+
+        case 3:
+            reset_leds();
+            printk(KERN_INFO "Reset mode activated: All LEDs turned OFF\n");
+            mode = -1;
+            individual_direction = 0;
+            printk(KERN_INFO "Previous mode cleared, ready for new mode\n");
+            break;
+    }
+
+    return IRQ_HANDLED;
+}
 
 static int __init led_module_init(void) {
     int ret, i;
@@ -63,6 +184,11 @@ static void __exit led_module_exit(void) {
 
     printk(KERN_INFO "LED Module Exit\n");
 
+    // 스레드가 존재하면 중지
+    if (led_thread) {
+        kthread_stop(led_thread);
+    }
+
     for (i = 0; i < NUM_LEDS; i++) {
         gpio_set_value(led_pins[i], 0);
         gpio_free(led_pins[i]);
@@ -74,88 +200,6 @@ static void __exit led_module_exit(void) {
     }
 
     printk(KERN_INFO "LED Module Exit Complete\n");
-}
-
-static void reset_leds(void) {
-    int i;
-    for (i = 0; i < NUM_LEDS; i++) {
-        gpio_set_value(led_pins[i], 0);
-    }
-}
-
-static void set_led(int led_idx, int value) {
-    if (led_idx >= 0 && led_idx < NUM_LEDS) {
-        gpio_set_value(led_pins[led_idx], value);
-    }
-}
-
-static irqreturn_t switch_handler(int irq, void *dev_id) {
-    int switch_id = (int)(long)dev_id;
-    int i;
-
-    printk(KERN_INFO "Switch %d pressed\n", switch_id);
-
-    mode = switch_id;
-
-    switch (mode) {
-        case 0:
-            reset_leds();
-            while (mode == 0) {
-                for (i = 0; i < NUM_LEDS; i++) {
-                    set_led(i, 1);
-                }
-                printk(KERN_INFO "All LEDs ON\n");
-                msleep(2000);
-
-                reset_leds();
-                printk(KERN_INFO "All LEDs OFF\n");
-                msleep(2000);
-            }
-            break;
-
-        case 1:
-            reset_leds();
-            if (individual_direction == 0) {
-                for (i = 0; i < NUM_LEDS; i++) {
-                    set_led(i, 1);
-                    printk(KERN_INFO "LED[%d] ON\n", i);
-                    msleep(2000);
-                    reset_leds();
-                }
-            } else {
-                for (i = NUM_LEDS - 1; i >= 0; i--) {
-                    set_led(i, 1);
-                    printk(KERN_INFO "LED[%d] ON\n", i);
-                    msleep(2000);
-                    reset_leds();
-                }
-            }
-            individual_direction = !individual_direction;
-            printk(KERN_INFO "Direction reversed: %s\n",
-                   individual_direction ? "Right-to-Left" : "Left-to-Right");
-            break;
-
-        case 2:
-            if (switch_id >= 0 && switch_id < 3) {
-                int led_index = switch_id;
-                int current_state = gpio_get_value(led_pins[led_index]);
-                set_led(led_index, !current_state);
-                printk(KERN_INFO "Manual mode: LED[%d] toggled to %d\n", led_pins[led_index], !current_state);
-            } else {
-                printk(KERN_WARNING "Manual mode: Invalid switch_id %d\n", switch_id);
-            }
-            break;
-
-        case 3:
-            reset_leds();
-            printk(KERN_INFO "Reset mode activated: All LEDs turned OFF\n");
-            mode = -1;
-            individual_direction = 0;
-            printk(KERN_INFO "Previous mode cleared, ready for new mode\n");
-            break;
-    }
-
-    return IRQ_HANDLED;
 }
 
 module_init(led_module_init);
